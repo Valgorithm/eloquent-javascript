08d82657329c795c5a5f8aebd093b128c6998459
Valerie Cavanaugh <valerieccavanaugh@gmail.com>
2018-01-17 17:42:44 -0500

    Create recursion-positive-whole-number.js
    Add solution to "Recursion" exercise found in "Eloquent JavaScript" Chapter 3, "Functions"

---
+++ recursion-positive-whole-number.js
@@ -0,0 +1,17 @@
+/*
+ The code below defines a recursive function that accepts a number parameter and returns a Boolean indicating whether the number, if
+ positive and whole, is even (true) or odd (false) according to the following rules:
+ 
+   • Zero is even.
+   • One is odd.
+   • For any other number N, its evenness is the same as N - 2.
+*/
+
+var isEven = function(number) {
+  if (number == 0 || number == 1)
+    return number == 0;
+  else
+    return isEven(number - 2);
+};
+
+console.log(isEven(Number(prompt("Enter a number:", ""))));
